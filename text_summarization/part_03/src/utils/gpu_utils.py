da38ad364e22c61ae6c04fd8695cf128cf97958a
Yan <ywan328@outlook.com>
2021-08-10 16:04:05 +0800

    Add files via upload

---
+++ text_summarization/part_03/src/utils/gpu_utils.py
@@ -0,0 +1,20 @@
+import os
+import tensorflow as tf
+
+
+def config_gpu(use_cpu=False, gpu_memory=6):
+    if use_cpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = ''
+    else:
+        os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        tf.config.experimental.set_memory_growth(gpus[0], enable=True)
+        if gpus:
+            try:
+                tf.config.experimental.set_virtual_device_configuration(
+                    gpus[0],
+                    [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024 * gpu_memory)])
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            except RuntimeError as e:
+                print(e)
